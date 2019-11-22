49874db4825b1e6170f90bf68fa17e2f38fd276e
cruzmayra <velazquez.leonora@gmail.com>
2019-11-22 00:02:55 -0600

    llamada api

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 const Home = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    var url = 'https://images-api.nasa.gov/search?q=all';
+ 
+    fetch( url )
+        .then( r => r.json() )
+        .then( data => setItems( data.collection.items ) )
+        .catch( e => console.error( 'Something went wrong' ) );
+      })
+
   return (
-    <div class="container is-fullhd">
-        <div class="notification">
+    <div className="container is-fullhd">
+        <div className="notification">
             This container is <strong>fullwidth</strong> <em>until</em> the <code>$fullhd</code> breakpoint.
         </div>
     </div>
